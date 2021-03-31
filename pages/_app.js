31966fc0492cd0d4397fee3be5f4e0ad4fea61f5
Kartik <kartikey.sri98@gmail.com>
2021-03-31 13:03:05 +0530

    add: react-query with graphql support

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import {useRef} from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {Hydrate} from 'react-query/hydration';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  const queryClientRef = useRef();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydrateState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {gql, GraphQLClient} from 'graphql-request';
+import {QueryClient, useQuery} from 'react-query';
+import {dehydrate} from 'react-query/hydration';
+
+const POPULAR_MOVIES = gql`
+  query {
+    popularMovies {
+      id
+      title 
+      overview
+      poster(imgSize: M)
+    }
+  }
+`;
+
+async function request(query) {
+  const client = new GraphQLClient("http://localhost:4000");
+  const data = await client.request(query);
+  return data;
+}
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery('popularMovies', async () => {
+    const data = await request(POPULAR_MOVIES);
+    return data.popularMovies;
+  });
+  return {
+    props: {
+      dehydrateState: dehydrate(queryClient)
+    }
+  }
+}
 
 export default function Home() {
+  const {data: popularMovies} = useQuery('popularMovies', async () => {
+    const data = await request(POPULAR_MOVIES);
+    return data.popularMovies;
+  });
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      {JSON.stringify(popularMovies)}
     </div>
   )
 }
