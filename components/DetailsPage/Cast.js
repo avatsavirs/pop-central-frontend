510627eaf6d545f3c14ae999cae29187fd458d0e
Kartik <kartikey.sri98@gmail.com>
2021-04-06 10:39:09 +0530

    add: movie details page cast links

--- components/DetailsPage/Cast.js
+++ components/DetailsPage/Cast.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import {CardImage} from 'styled-components';
 import {Card} from 'styled-components';
 
@@ -10,26 +11,38 @@ export default function Cast({cast}) {
       gap: '3rem',
       flexWrap: 'wrap',
       justifyContent: 'center',
-    }}>
+    }} tabIndex={-1}>
       {cast.map(person => (
-        <Card key={person.id} width="15rem" height="29rem">
-          <CardImage portion="80%" >
-            <Image src={person.photo || '/img/profile.png'} layout="fill" objectFit="cover" />
-          </CardImage>
-          <div css={{
-            flexBasis: '20%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            textAlign: 'center'
+        <Link key={person.id} href={`/person/${person.id}`} passHref>
+          <a css={{
+            textDecoration: 'none',
+            color: 'var(--primary-text)',
+            ':focus, :hover': {
+              color: 'var(--secondary)',
+              outline: '1px solid var(--secondary)',
+              '--muted-text': 'var(--secondary)'
+            },
           }}>
-            <p> {person.name} </p>
-            <p css={{
-              fontSize: 'var(--small)',
-              color: 'var(--muted-text)'
-            }}> {person.role} </p>
-          </div>
-        </Card>
+            <Card width="15rem" height="29rem">
+              <CardImage portion="80%" >
+                <Image src={person.photo || '/img/profile.png'} layout="fill" objectFit="cover" />
+              </CardImage>
+              <div css={{
+                flexBasis: '20%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                textAlign: 'center'
+              }}>
+                <p> {person.name} </p>
+                <p css={{
+                  fontSize: 'var(--small)',
+                  color: 'var(--muted-text)'
+                }}> {person.role} </p>
+              </div>
+            </Card>
+          </a>
+        </Link>
       ))}
     </div>
   )
