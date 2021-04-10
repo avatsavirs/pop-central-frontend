da9e495e12ade44c67acab606ef9498e2435e71a
Kartik <kartikey.sri98@gmail.com>
2021-04-10 13:03:49 +0530

    fix: graphql request error object

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,11 @@
 import {GraphQLClient} from 'graphql-request'
 export async function request(query) {
-  const client = new GraphQLClient("http://localhost:4000");
-  const data = await client.request(query);
-  return data;
+  try {
+    const client = new GraphQLClient("http://localhost:4000");
+    const data = await client.request(query);
+    return data;
+  } catch ({response}) {
+    throw response.errors[0];
+  }
 }
 
