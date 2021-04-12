4f70b33744bb4e70f5c3a7315cda7b8c36cc2505
Kartik <kartikey.sri98@gmail.com>
2021-04-12 09:07:49 +0530

    fix: graphql query variables

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,8 @@
 import {GraphQLClient} from 'graphql-request'
-export async function request(query) {
+export async function request(query, variables) {
   try {
     const client = new GraphQLClient("http://localhost:4000");
-    const data = await client.request(query);
+    const data = await client.request(query, variables);
     return data;
   } catch ({response}) {
     throw response.errors[0];
