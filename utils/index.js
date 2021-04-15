1e3d7829637a0e158b1dd5e62f7787485d34a7b2
Kartik <kartikey.sri98@gmail.com>
2021-04-15 09:16:43 +0530

    add: authRequest

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 import {GraphQLClient} from 'graphql-request'
+
 export async function request(query, variables) {
   try {
     const client = new GraphQLClient("http://localhost:4000");
@@ -9,3 +10,17 @@ export async function request(query, variables) {
   }
 }
 
+export async function authRequest(query, variables, accessToken, refreshToken) {
+  const client = new GraphQLClient("http://localhost:4000", {
+    headers: {
+      'x-access-token': accessToken,
+      'x-refresh-token': refreshToken
+    },
+  });
+  try {
+    const data = await client.request(query, variables);
+    return data;
+  } catch ({response}) {
+    throw response.errors[0];
+  }
+}
