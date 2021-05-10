f2fc332eb82a4d1b05dd295fb91fd6a28ca18169
Kartik <kartikey.sri98@gmail.com>
2021-05-10 10:57:31 +0530

    fix: login button styles

--- components/Nav.js
+++ components/Nav.js
@@ -32,7 +32,13 @@ export default function Nav() {
       </Link>
       <div css={{display: 'flex', gap: '2rem', alignItems: 'center'}}>
         <button aria-label={theme === 'dark' ? 'light-theme' : 'dark-theme'} css={{background: 'transparent', color: 'var(--primary-color)', border: 'none', cursor: 'pointer'}} onClick={switchTheme}>{theme === 'dark' ? <SunIcon css={{width: '3rem', height: '3rem'}} /> : <MoonIcon css={{width: '3rem', height: '3rem'}} />}</button>
-        {isSessionLoading ? <UserMenuPlaceholder /> : (isLoggedIn ? <UserMenu user={user} /> : <button onClick={() => signIn('google')}>Login</button>)}
+        {isSessionLoading ? <UserMenuPlaceholder /> : (isLoggedIn ? <UserMenu user={user} /> : <button onClick={() => signIn('google')} css={{
+          background: 'none',
+          border: 'none',
+          color: 'var(--primary-text)',
+          fontSize: 'var(--p)',
+          cursor: 'pointer'
+        }}>Login</button>)}
       </div>
     </nav>
   )
