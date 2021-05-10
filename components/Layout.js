57e0d227c685161106e2350be4d48ccc47990f73
Kartik <kartikey.sri98@gmail.com>
2021-05-10 10:58:08 +0530

    add: title bar icon

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Footer from './Footer';
 import Nav from './Nav';
 
 export default function Layout({children}) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/img/p_logo.png" type = "image/x-icon"/>
+      </Head>
       <Nav />
       <main css={{
         flexGrow: 1,
